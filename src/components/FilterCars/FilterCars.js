ca6a00b9db013d39b1d489b30073b1ee168e759e
Alyosha97 <160518126+Alyosha97@users.noreply.github.com>
2024-08-22 13:49:58 +0400

    second commit

--- src/components/FilterCars/FilterCars.js
+++ src/components/FilterCars/FilterCars.js
@@ -10,7 +10,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { years, prices } from "./Helper";
+import { years, prices } from "./helper";
 import { useEffect, useState } from "react";
 
 const FilterCars = () => {
